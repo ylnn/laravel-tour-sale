6acb9fd48856a546babde4ba159088a3b74c442a
yalin <yalincobanoglu@yandex.com>
2018-03-02 20:17:32 +0300

    admin route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::group(['prefix' => 'admin'], function () { /* Middleware: admin eklenecek */
+
+    Route::get('index', 'Admin\AdminController@index');
+
+});
