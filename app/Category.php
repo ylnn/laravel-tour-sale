b59919e196f4d825bb194903fd5b99ab27947581
yalin <yalincobanoglu@yandex.com>
2018-03-02 23:13:23 +0300

    category model

---
+++ app/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+}
