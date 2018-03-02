ec7c3df7d0d6f418c2bda6aaffbf33fb6324c27c
yalin <yalincobanoglu@yandex.com>
2018-03-02 23:06:49 +0300

    category controller

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        return view('admin.category.index');
+    }
+
+    public function create()
+    {
+        return view('admin.category.create');   
+    }
+
+    public function edit()
+    {
+        return view('admin.category.edit');   
+    }
+}
