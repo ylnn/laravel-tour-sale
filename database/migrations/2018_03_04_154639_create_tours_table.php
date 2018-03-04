2fc7af461800234a72c85ada056b4746fba9db25
yalin <yalincobanoglu@yandex.com>
2018-03-04 21:05:57 +0300

    tours table migration

---
+++ database/migrations/2018_03_04_154639_create_tours_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateToursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tours', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->longText('description');
+            $table->integer('min_participant');
+            $table->integer('max_participant');
+            $table->integer('price');
+            $table->integer('single_price');
+            $table->string('currency');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tours');
+    }
+}
