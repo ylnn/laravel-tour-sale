1939e02b946dca9187c81adac5bb4e330c596d43
yalin <yalincobanoglu@yandex.com>
2018-03-04 23:45:51 +0300

    dates migration

---
+++ database/migrations/2018_03_04_203230_create_dates_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->longText('description');
+            $table->integer('min_participant');
+            $table->integer('max_participant');
+            $table->integer('price');
+            $table->integer('single_price');
+            $table->string('currency');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dates');
+    }
+}
