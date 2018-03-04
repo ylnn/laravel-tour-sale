3c1e1a0724e149486b62e1460548ab7ec38e243c
yalin <yalincobanoglu@yandex.com>
2018-03-04 23:45:16 +0300

    tour model

--- app/Tour.php
+++ app/Tour.php
@@ -3,8 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tour extends Model
 {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    /* slug belirleme */
+    public function setSlug($slug)
+    {
+        if (!empty($slug)) {
+            $this->slug = str_slug($slug);
+            return;
+        }
+
+        if (isset($this->name)) {
+            $this->slug = str_slug($this->name);
+        }
+    }
     //
 }
