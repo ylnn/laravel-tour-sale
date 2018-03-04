50e1473ddc776e025e6932495e20ce0282a7ec61
yalin <yalincobanoglu@yandex.com>
2018-03-04 21:06:10 +0300

    tour routes

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,15 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () { /* Mid
         Route::post('update/{category}', 'CategoryController@update')->name('admin.category.update');
         Route::get('delete/{category}', 'CategoryController@destroy')->name('admin.category.delete');
     });
+    
+    Route::group(['prefix' => 'tour'], function(){
+        Route::get('index', 'TourController@index')->name('admin.tour.index');
+        Route::get('create', 'TourController@create')->name('admin.tour.create');
+        Route::post('store', 'TourController@store')->name('admin.tour.store');
+        Route::get('show/{tour}', 'TourController@show')->name('admin.tour.show');
+        Route::get('edit/{tour}', 'TourController@edit')->name('admin.tour.edit');
+        Route::post('update/{tour}', 'TourController@update')->name('admin.tour.update');
+        Route::get('delete/{tour}', 'TourController@destroy')->name('admin.tour.delete');
+    });
 
 });
