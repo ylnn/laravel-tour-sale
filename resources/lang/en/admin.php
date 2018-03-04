f8f9cf5a3172a058d2b6e766c355601cfe79dbc5
yalin <yalincobanoglu@yandex.com>
2018-03-04 21:41:59 +0300

    admin lang file

---
+++ resources/lang/en/admin.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /* Ortak */
+    'durum' => 'Durum',
+    'yeni' => 'Yeni',
+    'yayinda' => 'Yayında',
+    'yayinda-degil' => 'Yayında',
+    'aciklama' => 'Açıklama',
+    'kaydet' => 'Kaydet',
+    'iptal' => 'İptal',
+    'filtrele' => 'Filtrele',
+    'isim' => 'İsim',
+    'ekleme' => 'Ekleme',
+    'guncelleme' => 'Güncelleme',
+    'islemler' => 'İşlemler',
+    'goster' => 'Göster',
+    'duzenle' => 'Düzenle',
+    'sil' => 'Sil',
+    'bulunamadi' => 'Bulunamadı',
+
+
+    /* Kategori */
+    'kategori' => 'Kategori',
+    'kategoriler' => 'Kategoriler',
+    'kategori-adi' => 'Kategori Adı',
+    'kategori-seo-adi' => 'Kategori seo adı | Örnek: "gunluk-turlar" | boş bırakılabilir',
+    'kategori-olustur' => 'Kategori Oluştur',
+
+];
