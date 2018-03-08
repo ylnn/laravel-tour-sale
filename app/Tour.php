79896ea599edc6acc7a059268e9c77ab95613c5c
yalin <yalincobanoglu@yandex.com>
2018-03-08 22:29:47 +0300

    tour model date relation

--- app/Tour.php
+++ app/Tour.php
@@ -4,12 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Date;
 
 class Tour extends Model
 {
     use SoftDeletes;
 
     protected $dates = ['deleted_at'];
+
+    public function dates()
+    {
+        return $this->hasMany(Date::class);   
+    }
 
     /* slug belirleme */
     public function setSlug($slug)
