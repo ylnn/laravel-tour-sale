bb7aa993d49f71adc21a76163adc1b5ab0af84a5
yalin <yalincobanoglu@yandex.com>
2018-03-08 22:30:37 +0300

    tours table seeder

---
+++ database/seeds/ToursTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Tour;
+
+class ToursTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Tour::class, 50)->create();
+    }
+}
