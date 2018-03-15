6e4795bf1783efe9af89dd749402ec395e449cf7
yalin <yalincobanoglu@yandex.com>
2018-03-15 13:36:02 +0300

    tour.index blade updated

--- resources/views/admin/tour/index.blade.php
+++ resources/views/admin/tour/index.blade.php
@@ -9,15 +9,10 @@
         <div class="btn-group mr-2">
         <a href="{{ route('admin.tour.create') }}" class="btn btn-sm btn-outline-secondary">+ @lang('admin.yeni')</a>
         </div>
-        {{--  <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
-          This week
-        </button>  --}}
       </div>
     </div>
 
     <div class="">
-{{--          <table class="table table-striped table-sm">  --}}
         <table id="datatable" class="table table-striped table-bordered" width="100%" cellspacing="0">
           <thead>
             <tr>
