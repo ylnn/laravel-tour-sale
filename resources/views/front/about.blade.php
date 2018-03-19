e5bbd18e2144a63a25534183ef7111cca11410ac
yalin <yalincobanoglu@yandex.com>
2018-03-19 20:04:19 +0300

    about.blade deleted

--- resources/views/front/about.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('front')
-
-@section('content')
-    <h2>About</h2>
-@endsection
---
+++ resources/views/front/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.front')
+
+@section('content')
+    <h2>Home</h2>
+    <p>home contentr</p>
+@endsection
