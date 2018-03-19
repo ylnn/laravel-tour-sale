ce7b5debd09c6f91ae6a5dfc9341ef73c93f8fec
yalin <yalincobanoglu@yandex.com>
2018-03-19 22:04:12 +0300

    database seeeder updated

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call('ToursTableSeeder');
+        // $this->call('ToursTableSeeder');
+        $this->call('CategoriesTableSeeder');
     }
 }
