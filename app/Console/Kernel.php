90a71b6e63bca30f45c19c163e6376a41cd3cb71
yalin <yalincobanoglu@yandex.com>
2018-04-01 11:26:50 +0300

    kernel.php scheduler example

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,6 +27,10 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        // $schedule->call(function(){
+            // Log::info('info from schedule! 2 dakika');
+            // dispatch(new CreateNewCategory());
+        // })->everyMinute();
     }
 
     /**
