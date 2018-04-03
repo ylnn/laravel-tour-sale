e7541e510769eeb4f86bd916b4591c6c8db8b3f0
yalin <yalincobanoglu@yandex.com>
2018-04-03 18:19:21 +0300

    category blade

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -4,10 +4,21 @@
     <h2>{{$category->name}}</h2>
 
     @isset($tours)
-        <ul>
+        <ul class="tour-list">
             @foreach ($tours as $tour)
-                <li>
-                    {{$tour->name}}
+                <li class="border">
+                    <div class="row">
+                        <div class="col-2">
+                            <img src="holder.js/170x120">
+                        </div>
+                        <div class="col">
+                            <h5>{{$tour->name}}</h5>
+                            <p>{{$tour->summary}}</p>
+                            <p>
+                                <a name="" id="" class="btn btn-primary" href="#" role="button">Tarihleri göster</a>
+                            </p>
+                        </div>
+                    </div>
                 </li>
             @endforeach
         </ul>
