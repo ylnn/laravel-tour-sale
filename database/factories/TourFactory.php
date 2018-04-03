12057d960374c1402b1a00c770a4c67220d0cc24
yalin <yalincobanoglu@yandex.com>
2018-04-03 18:47:14 +0300

    tour factory updated

--- database/factories/TourFactory.php
+++ database/factories/TourFactory.php
@@ -6,9 +6,10 @@ use App\Tour;
 $factory->define(Tour::class, function (Faker $faker) {
     return [
         'status' => 1,
-        'name' => $faker->sentence(),
+        'name' => $faker->sentence(3),
         'slug' => str_slug($faker->sentence()),
         'description' => $faker->paragraph(),
         'summary' => $faker->paragraph(),
+        'category_id' => rand(1,3)
     ];
 });
