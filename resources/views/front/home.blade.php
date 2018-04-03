68f8d549de51141f18b10b6aba5d7cb39effa3e7
yalin <yalincobanoglu@yandex.com>
2018-04-03 18:42:31 +0300

    home.blade updated

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,6 +1,21 @@
 @extends('layout.front')
 
 @section('content')
-    <h2>Home</h2>
-    <p>home contentr</p>
+    <h2>Popular Tours</h2>
+    <div class="popularTours">
+        <ul>
+            <li>
+                <div class="border">
+                    <img src="holder.js/300x200">
+                    <p>Culturel Tour</p>
+                </div>
+        </li><li>
+                <div class="border">
+                    <img src="holder.js/300x200">
+                    <p>Culturel Tour</p>
+                </div>
+        </li>
+    </ul>
+</div>
+
 @endsection
