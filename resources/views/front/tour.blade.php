b5ea3e2a5bedfdbb6586e43b5578dc170629d615
yalin <yalincobanoglu@yandex.com>
2018-04-04 14:02:06 +0300

    front tour.blade

--- resources/views/front/tour.blade.php
+++ resources/views/front/tour.blade.php
@@ -1,5 +1,11 @@
 @extends('layout.front') 
 
 @section('content')
-    <h2>{{$category->name}}</h2>
+<h2>{{$tour->name}}</h2>
+<img src="holder.js/370x220">
+<p>{{ $tour->summary }}</p>
+
+<h4>Dates:</h4>
+
+{{ $tour->dates }}
 @endsection
