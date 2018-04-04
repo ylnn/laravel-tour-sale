85c7dca979c2d49ed3efc156745b04dc703e3177
yalin <yalincobanoglu@yandex.com>
2018-04-04 14:01:14 +0300

    front tour controller

--- app/Http/Controllers/Front/TourController.php
+++ app/Http/Controllers/Front/TourController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Front;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Category;
+use App\Tour;
 
 class TourController extends Controller
 {
-    public function show()
+    public function show(Tour $tour)
     {
-        return 'tour-detail';
-        // return view('front.home');
+        // return 'tour-detail ' . request('tour');
+        return view('front.tour', compact('tour'));
     }
 }
