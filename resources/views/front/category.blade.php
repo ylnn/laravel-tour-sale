9d5d59c6aa52306c04f3c2c0c06b77f982c2a40e
yalin <yalincobanoglu@yandex.com>
2018-04-04 14:02:00 +0300

    front categoryb.lade

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -3,19 +3,23 @@
 @section('content')
     <h2>{{$category->name}}</h2>
 
+
     @isset($tours)
         <ul class="tour-list">
             @foreach ($tours as $tour)
+                @php
+                    $tourDetailLink = route('tour', [$tour->id])
+                @endphp
                 <li class="border">
                     <div class="row">
                         <div class="col-2">
                             <img src="holder.js/170x120">
                         </div>
                         <div class="col">
-                            <h5>{{$tour->name}}</h5>
+                            <h5><a href="{{$tourDetailLink}}">{{$tour->name}}</a></h5>
                             <p>{{$tour->summary}}</p>
                             <p>
-                                <a name="" id="" class="btn btn-primary" href="#" role="button">Tarihleri göster</a>
+                                <a name="" id="" class="btn btn-primary" href="{{$tourDetailLink}}" role="button">Tarihleri göster</a>
                             </p>
                         </div>
                     </div>
