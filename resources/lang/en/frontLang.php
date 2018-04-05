e51c1d1d6f854765bff38988a12d1be0fe6b844d
yalin <yalincobanoglu@yandex.com>
2018-04-05 13:31:41 +0300

    frontlang

---
+++ resources/lang/en/frontLang.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+
+    /* Ortak */
+    'durum' => 'Durum',
+    'yeni' => 'Yeni',
+    'yayinda' => 'Yayında',
+    'yayinda-degil' => 'Yayında Değil',
+    'aciklama' => 'Açıklama',
+    'kaydet' => 'Kaydet',
+    'iptal' => 'İptal',
+    'filtrele' => 'Filtrele',
+    'isim' => 'İsim',
+    'ekleme' => 'Ekleme',
+    'guncelleme' => 'Güncelleme',
+    'islemler' => 'İşlemler',
+    'goster' => 'Göster',
+    'duzenle' => 'Düzenle',
+    'sil' => 'Sil',
+    'bulunamadi' => 'Bulunamadı',
+    'olustur' => 'Oluştur',
+    'search' => 'Ara',
+    'bosolabilir' => 'Boş bırakılabilir',
+
+
+    /* Kategori */
+    'kategori' => 'Kategori',
+    'kategoriler' => 'Kategoriler',
+    'kategori-adi' => 'Kategori Adı',
+    'kategori-seo-adi' => 'Kategori seo adı | Örnek: "gunluk-turlar" | boş bırakılabilir',
+    'kategori-olustur' => 'Kategori Oluştur',
+
+    /* Tur */
+    'tur' => 'Tur',
+    'turlar' => 'Turlar',
+    'tur-adi' => 'Tur Adı',
+    'tur-aciklamasi-kisa' => 'Tur Açıklaması (kısa)',
+    'tur-aciklamasi-tamami' => 'Tur Açıklaması (tamamı)',
+    'tur-seo-adi' => 'Tur seo adı | Örnek: "istanbul-sehir-turu" | boş bırakılabilir',
+    'min-katilimci' => 'Minimum katılımcı sayısı',
+    'max-katilimci' => 'Maximum katılımcı sayısı',
+    'fiyat' => 'Fiyat',
+    'single-fiyat' => 'Single Fiyat',
+    'para-birimi' => 'Para Birimi',
+
+    /* Tarihler */
+    'tarihler' => 'Tarihler',
+    'tarih' => 'Tarih',
+    'tarih-olustur' => 'Tarih Oluştur',
+    'baslama-tarihi' => 'Başlama Tarihi',
+    'bitis-tarihi' => 'Bitiş Tarihi',
+
+];
