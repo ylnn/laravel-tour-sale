c704d219d6c06269aa5dc493042e7a99330632dc
yalin <yalincobanoglu@yandex.com>
2018-04-05 13:23:08 +0300

    tour.blade

--- resources/views/front/tour.blade.php
+++ resources/views/front/tour.blade.php
@@ -3,9 +3,31 @@
 @section('content')
 <h2>{{$tour->name}}</h2>
 <img src="holder.js/370x220">
-<p>{{ $tour->summary }}</p>
+<p>{!!$tour->summary!!}</p>
 
 <h4>Dates:</h4>
-
-{{ $tour->dates }}
+<div class="row">
+    <div class="col-8">
+        @isset($tour->dates)
+        <table class="table">
+            <thead>
+                <th>@lang('frontLang.baslama-tarihi')</th>
+                <th>@lang('frontLang.bitis-tarihi')</th>
+                <th>@lang('frontLang.fiyat')</th>
+                <th>@lang('frontLang.single-fiyat')</th>
+                <th>@lang('frontLang.islemler')</th>
+            </thead>
+            @foreach($tour->dates as $date)
+                <tr>
+                    <td>{{$date->start_date->format('d/m/Y')}}</td>
+                    <td>{{$date->end_date->format('d/m/Y')}}</td>
+                    <td>{{$date->price}} {{ $date->currency }}</td>
+                    <td>{{$date->single_price}} {{ $date->currency }}</td>
+                    <td><a name="" id="" class="btn btn-info" href="#" role="button">Rezervasyon Yap</a></td>
+                </tr>
+            @endforeach
+        </table>
+        @endif
+    </div>
+</div>
 @endsection
