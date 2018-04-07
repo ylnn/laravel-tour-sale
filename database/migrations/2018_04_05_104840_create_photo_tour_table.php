ed359866446947052781e7df8b5f5400e29ebb7b
yalin <yalincobanoglu@yandex.com>
2018-04-07 18:54:51 +0300

    photo_tour table migration

---
+++ database/migrations/2018_04_05_104840_create_photo_tour_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotoTourTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photo_tour', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('photo_id');
+            $table->unsignedInteger('tour_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photo_tour');
+    }
+}
