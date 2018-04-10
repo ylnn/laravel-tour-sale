87e2cd2d0e835f87ef40313040f07ad0a4a37445
yalin <yalincobanoglu@yandex.com>
2018-04-10 10:50:03 +0300

    photo controller

--- app/Http/Controllers/Admin/PhotoController.php
+++ app/Http/Controllers/Admin/PhotoController.php
@@ -12,54 +12,122 @@ use Intervention\Image\ImageManagerStatic as Image;
 
 class PhotoController extends Controller
 {
+    protected $mainFolder = 'public/';
+
     public function form()
     {
        return view('admin.form');
     }
-    public function upload(Request $request)
+    public function upload(Request $request, $folder = 'tour_images')
     {
-        $validator = Validator::make($request->all(), [
-            'file' => [
+        if($this->validator($request)->fails()) {
+            // return response()->json('validation error', 403);
+            return false;
+        }
+
+        $paths = $this->getPathImage($request);
+
+        list($filename, $filename_thumb, $photoRealPath) = $paths;
+
+        //set image storage folder
+        $path = $folder . '/' . $filename;
+        $path_thumb = $folder . '/' . $filename_thumb;
+
+
+        if(!$this->makeAndStoreImage($photoRealPath, $path)){
+            return false;
+        }
+
+        if(!$this->makeAndStoreImageThumb($photoRealPath, $path_thumb)){
+            return false;
+        }
+
+        $photo = $this->savePhotoToDB($path, $path_thumb);
+        if(!$photo){
+            return false;
+        }
+        return $photo->id;
+    }
+
+    protected function validator(Request $request)
+    {
+        return Validator::make($request->all(), [
+            'photo' => [
                 'required',
                 Rule::dimensions()->maxWidth(3000)->maxHeight(2000),
             ],
         ]);
+    }
 
-        if($validator->fails()) {
-            return response()->json('validation error');
+    protected function makeAndStoreImage($photoRealPath, $path)
+    {
+        //make image
+        $image = Image::make($photoRealPath)->fit(700, 400, function ($constraint) {
+            $constraint->upsize();
+        });
+        if(!$image){
+            return false;
+        }
+        //store image
+        $storedFile = Storage::put($this->mainFolder . $path, (string)$image->encode());
+        if(!$storedFile){
+            return false;
+        }
+        return true;
+    }
+
+    protected function makeAndStoreImageThumb($photoRealPath, $path_thumb)
+    {
+        // make thumb image
+        $image_thumb = Image::make($photoRealPath)->fit(300, null, function ($constraint) {
+            $constraint->upsize();
+        });
+
+        if(!$image_thumb) {
+            return false;
         }
 
-        $photo = $request['file'];
+        //store thumb image
+        $storedFile_thumb = Storage::put($this->mainFolder . $path_thumb, (string) $image_thumb->encode());
 
+        if(!$storedFile_thumb) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function getPathImage($request)
+    {
+        //file        
+        $photo = $request['photo'];
+
+        //get file details
         $photoOrginalName = $photo->getClientOriginalName();
         $photoExt = $photo->getClientOriginalExtension();
         $photoRealPath = $photo->getRealPath();
+
+        //make filename with random
 
         $random = str_random(40);
         $filename = $random . '.' . $photoExt;
         $filename_thumb = $random . '_thumb.' . $photoExt;
 
-        $path = 'public/tour_images/' . $filename;
-        $path_thumb = 'public/tour_images/' . $filename_thumb;
+        return array($filename, $filename_thumb, $photoRealPath);
 
-        $image = Image::make($photoRealPath)->fit(700, 400, function ($constraint) {
-            $constraint->upsize();
-        });
+    }
 
-        $storedFile = Storage::put($path, (string) $image->encode());
-
-
-        // Thumb Start
-        $image_thumb = Image::make($photoRealPath)->fit(300, null, function ($constraint) {
-            $constraint->upsize();
-        });
-        $storedFile_thumb = Storage::put($path_thumb, (string) $image_thumb->encode());
-
+    protected function savePhotoToDB($path, $path_thumb)
+    {
         $photo = new Photo;
         $photo->filename = $path;
         $photo->filename_thumb = $path_thumb;
-        $photo->save();
 
-        return 'OK';
+        if(!$photo->save()){
+            return false;
+        }
+
+        return $photo;
     }
+
 }
