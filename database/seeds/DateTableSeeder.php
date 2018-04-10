3ff1437754fca3418d27946e4e0f6c1838cd4357
yalin <yalincobanoglu@yandex.com>
2018-04-10 11:10:28 +0300

    date seeder

---
+++ database/seeds/DateTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class DateTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         DB::table('dates')->insert([
+            'status' => 1,
+            'tour_id' => 1,
+            'user_id' => 1,
+            'start_date' => Carbon::now()->addDays(10),
+            'end_date' => Carbon::now()->addDays(20),
+            'min_participant' => 10,
+            'max_participant' => 20,
+            'price' => 1950,
+            'currency' => 'USD',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
