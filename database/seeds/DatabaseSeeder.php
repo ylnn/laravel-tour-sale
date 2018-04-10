5e12a0325aab86b3be4a22a3a40b9aa56c1c7053
yalin <yalincobanoglu@yandex.com>
2018-04-10 11:10:33 +0300

    db seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call('CategoriesTableSeeder');
         $this->call('ToursTableSeeder');
+        $this->call('DateTableSeeder');
     }
 }
