fff7d6052ced6329fa7f0cc8472f4d0ee6e924e3
yalin <yalincobanoglu@yandex.com>
2018-04-10 11:10:47 +0300

    tours table seeder

--- database/seeds/ToursTableSeeder.php
+++ database/seeds/ToursTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Tour;
+use Carbon\Carbon;
 
 class ToursTableSeeder extends Seeder
 {
@@ -13,5 +14,16 @@ class ToursTableSeeder extends Seeder
     public function run()
     {
         // factory(Tour::class, 50)->create();
+         DB::table('tours')->insert([
+            'status' => 1,
+            'category_id' => 1,
+            'name' => '3 Günlük karadeniz turu',
+            'slug' => '3-gun-karadeniz',
+            'description' => 'Karadeniz Tour Açıklama',
+            'summary' => 'Karadeniz Tour Açıklama Kısa',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 }
+
