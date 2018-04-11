b929e1e15ee8eecfd303f4eb80ed66f84fd048e2
yalin <yalincobanoglu@yandex.com>
2018-04-11 11:16:11 +0300

    home.blade

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,20 +1,25 @@
-@extends('layout.front')
+@extends('layout.front') 
 
 @section('content')
-    <h2>Popular Tours</h2>
-    <div class="popularTours">
-        <ul>
-            <li>
-                <div class="border">
-                    <img src="holder.js/300x200">
-                    <p>Culturel Tour</p>
-                </div>
-        </li><li>
-                <div class="border">
-                    <img src="holder.js/300x200">
-                    <p>Culturel Tour</p>
-                </div>
+<h2>Popular Tours</h2>
+<div class="popularTours">
+    <ul>
+        @foreach ($populars as $popular)
+        <li>
+            <div class="border">
+                @if(isset($popular->photos[0]))
+                    <img width="300" src="{{ asset('storage/' . $popular->photos[0]->filename ) }}">
+                @else
+                    <img src="holder.js/370x220">
+                @endif
+                <p>{{$popular->name}}</p>
+                @php 
+                    $tourDetailLink = route('tour', [$popular->id]) 
+                @endphp
+                <a class="btn btn-primary" href="{{$tourDetailLink}}" role="button">Tarihleri göster</a>
+            </div>
         </li>
+        @endforeach
     </ul>
 </div>
 
