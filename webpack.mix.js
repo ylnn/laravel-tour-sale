a4e6e6bb2690f8bcd2121c8c2357a8468d9a0264
yalin <yalincobanoglu@yandex.com>
2018-04-21 16:34:04 +0300

    web pack

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,5 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-    .js('resources/assets/js/reservation.js', 'public/js')
+    .js('resources/assets/js/price.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
