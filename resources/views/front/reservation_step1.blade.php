8dc18737f177d116a30c370c1818498fb6e10bc6
yalin <yalincobanoglu@yandex.com>
2018-04-21 20:01:34 +0300

    res step1 blade

---
+++ resources/views/front/reservation_step1.blade.php
@@ -0,0 +1,50 @@
+@extends('layout.front') 
+
+@push('footer_js')
+    <script src="{{asset('js/price.js')}}"></script>
+@endpush
+
+@section('content')
+
+<div class="row justify-content-center">
+    <div class="col-8">
+        @include('front.includes.tour_info')
+
+        <table class="table">
+            <thead>
+                <th>@lang('frontLang.baslama-tarihi')</th>
+                <th>@lang('frontLang.bitis-tarihi')</th>
+                <th>@lang('frontLang.fiyat') (@lang('frontLang.kisi-basi'))</th>
+            </thead>
+                <tr>
+                    <td>{{$date->start_date->format('d/m/Y')}}</td>
+                    <td>{{$date->end_date->format('d/m/Y')}}</td>
+                    <td>{{ $date->currency }} {{$date->price}}</td>
+                </tr>
+        </table>
+    </div>
+</div>
+{{-- <div id="reservation">
+    <reservation-form-component date_id = "123"></reservation-form-component>
+</div> --}}
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Please select participant count</div>
+                        <div class="card-body">
+                            <div id="price">
+                                <price-component 
+                                    date_id={{$date->id}} 
+                                    price={{$date->price}} 
+                                    currency={{$date->currency}}
+                                    step2_url={{url('reservation_step2')}}
+                                    >
+                                </price-component>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
