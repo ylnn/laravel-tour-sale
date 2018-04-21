ea7bdf7a408e7537e5529453178f810d48e7d98a
yalin <yalincobanoglu@yandex.com>
2018-04-21 08:38:11 +0300

    res show blade

--- resources/views/front/reservation_show.blade.php
+++ resources/views/front/reservation_show.blade.php
@@ -1,31 +1,39 @@
 @extends('layout.front') 
 
-@section('content')
-<h2>{{$tour->name}}</h2>
-<img src="holder.js/370x220">
-<p>{!!$tour->summary!!}</p>
+@push('footer_js')
+    <script src="{{asset('js/reservation.js')}}"></script>
+@endpush
 
-<h4>Dates:</h4>
-<div class="row">
+@section('content')
+
+<div class="row justify-content-center">
     <div class="col-8">
+        <div class="tour-info">
+            <h2>{{$tour->name}}</h2>
+            @if(isset($tour->photos[0]))
+                <img width="300" src="{{ asset('storage/' . $tour->photos[0]->filename ) }}">
+            @else
+                <img src="holder.js/370x220">
+            @endif
+            <p>{!!$tour->summary!!}</p>
+        </div>
+
         <table class="table">
             <thead>
                 <th>@lang('frontLang.baslama-tarihi')</th>
                 <th>@lang('frontLang.bitis-tarihi')</th>
-                <th>@lang('frontLang.fiyat')</th>
-                <th>@lang('frontLang.single-fiyat')</th>
+                <th>@lang('frontLang.fiyat') (@lang('frontLang.kisi-basi'))</th>
             </thead>
                 <tr>
                     <td>{{$date->start_date->format('d/m/Y')}}</td>
                     <td>{{$date->end_date->format('d/m/Y')}}</td>
                     <td>{{$date->price}} {{ $date->currency }}</td>
-                    <td>{{$date->single_price}} {{ $date->currency }}</td>
                 </tr>
         </table>
     </div>
 </div>
-<div id="app">
-    <reservation-form-component></reservation-form-component>
+<div id="reservation">
+    <reservation-form-component date_id = "123"></reservation-form-component>
 </div>
 
 @endsection
