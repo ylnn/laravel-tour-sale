8580df8c62843fc961071e17aa9780aa181fcda1
yalin <yalincobanoglu@yandex.com>
2018-04-24 11:16:11 +0300

    category.blade

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -1,30 +1,31 @@
-@extends('layout.front')
+@extends('layout.front') @section('content')
 
-@section('content')
-    <h2>{{$category->name}}</h2>
-
-
-    @isset($tours)
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <h2>{{$category->name}}</h2>
+        @isset($tours)
         <ul class="tour-list">
-            @foreach ($tours as $tour)
-                @php
-                    $tourDetailLink = route('tour', [$tour->id])
-                @endphp
-                <li class="border">
-                    <div class="row">
-                        <div class="col-2">
-                            <img src="holder.js/170x120">
-                        </div>
-                        <div class="col">
-                            <h5><a href="{{$tourDetailLink}}">{{$tour->name}}</a></h5>
-                            <p>{!!$tour->summary!!}</p>
-                            <p>
-                                <a name="" id="" class="btn btn-primary" href="{{$tourDetailLink}}" role="button">Tarihleri göster</a>
-                            </p>
-                        </div>
+            @foreach ($tours as $tour) @php $tourDetailLink = route('tour', [$tour->id]) @endphp
+            <li class="border">
+                <div class="row">
+                    <div class="col-2">
+                        <img src="holder.js/170x120">
                     </div>
-                </li>
+                    <div class="col">
+                        <h5>
+                            <a href="{{$tourDetailLink}}">{{$tour->name}}</a>
+                        </h5>
+                        <p>{!!$tour->summary!!}</p>
+                        <p>
+                            <a name="" id="" class="btn btn-primary" href="{{$tourDetailLink}}" role="button">Tarihleri göster</a>
+                        </p>
+                    </div>
+                </div>
+            </li>
             @endforeach
         </ul>
-    @endisset
+        @endisset
+    </div>
+</div>
+
 @endsection
