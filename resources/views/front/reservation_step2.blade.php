ea7f871bb669e39f2f4acfbf368c93381342f3a3
yalin <yalincobanoglu@yandex.com>
2018-04-24 11:16:40 +0300

    res step2 forms

--- resources/views/front/reservation_step2.blade.php
+++ resources/views/front/reservation_step2.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.front') 
 
 @push('footer_js')
-    <script src="{{asset('js/price.js')}}"></script>
+    {{-- <script src="{{asset('js/reservation.js')}}"></script> --}}
 @endpush
 
 @section('content')
@@ -26,6 +26,60 @@
                     <td>{{$currency}} {{$total_price}}</td>
                 </tr>
         </table>
+
+        <div class="card card-default">
+            <div class="card-body">
+                
+                <form method="POST" action="{{url('post')}}">
+
+                <h3 class="res-form-header">İletişim Bilgileri</h3>
+                <div class="form-group">
+                  <label for="">Ad Soyad:</label>
+                  <input type="text" name="name" id="name" class="form-control" placeholder="">
+                </div>
+                <div class="form-group">
+                  <label for="">E-posta Adresi:</label>
+                  <input type="text" name="email" id="email" class="form-control" placeholder="">
+                </div>
+
+                <div class="form-group">
+                  <label for="address">Adres:</label>
+                  <textarea class="form-control" name="address" id="address" rows="3"></textarea>
+                </div>
+
+                <div style="margin-bottom:50px"></div>
+
+                <h3 class="res-form-header">Katılımcı Bilgileri</h4>
+                <br>
+                    {{ csrf_field() }}
+                    @for ($i = 0; $i < $adult; $i++)
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-2"><label for="">Katılımcı:</label></div>
+                                <div class="col-md-3">
+                                    <select name="gender[{{$i}}]" id="gender{{$i}}" class="form-control">
+                                        <option value="mr">Mr.</option>
+                                        <option value="mrs">Mrs.</option>
+                                        <option value="miss">Ms.</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-5">
+                                    <input type="text" name="participant[{{$i}}]" id="participant{{$i}}" class="form-control" placeholder="Ad Soyad" aria-describedby="helpId">
+                                </div>
+                            </div>
+                            
+                        </div>
+                    @endfor
+                    <div class="row">
+                        <div class="col align-self-end">
+                            <button type="submit" class="btn btn-primary btn-lg">Gönder</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        
     </div>
 </div>
 
