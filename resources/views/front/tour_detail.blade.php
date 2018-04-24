93fbd911764b6e64bd90103950a343fd375e8b80
yalin <yalincobanoglu@yandex.com>
2018-04-24 17:24:18 +0300

    tour detail blade

--- resources/views/front/tour_detail.blade.php
+++ resources/views/front/tour_detail.blade.php
@@ -1,17 +1,13 @@
-@extends('layout.front') 
-
-@section('content')
-<h2>{{$tour->name}}</h2>
-    @if(isset($tour->photos[0]))
-        <img width="300" src="{{ asset('storage/' . $tour->photos[0]->filename ) }}">
-    @else
-        <img src="holder.js/325x200">
-    @endif
-<p>{!!$tour->summary!!}</p>
-
-<h4>Dates:</h4>
-<div class="row">
+@extends('layout.front') @section('content')
+<div class="row justify-content-center">
     <div class="col-8">
+        <h2>{{$tour->name}}</h2>
+        @if(isset($tour->photos[0]))
+        <img src="{{ asset('storage/' . $tour->photos[0]->filename ) }}"> @else
+        <img src="holder.js/325x200"> @endif
+        <p>{!!$tour->summary!!}</p>
+
+        <h4>Dates:</h4>
         @isset($tour->dates)
         <table class="table">
             <thead>
@@ -22,13 +18,15 @@
                 <th>@lang('frontLang.islemler')</th>
             </thead>
             @foreach($tour->dates as $date)
-                <tr>
-                    <td>{{$date->start_date->format('d/m/Y')}}</td>
-                    <td>{{$date->end_date->format('d/m/Y')}}</td>
-                    <td>{{$date->price}} {{ $date->currency }}</td>
-                    <td>{{$date->single_price}} {{ $date->currency }}</td>
-                    <td><a name="" id="" class="btn btn-info" href="{{route('reservation.show', [$date->id])}}" role="button">Rezervasyon Yap</a></td>
-                </tr>
+            <tr>
+                <td>{{$date->start_date->format('d/m/Y')}}</td>
+                <td>{{$date->end_date->format('d/m/Y')}}</td>
+                <td>{{$date->price}} {{ $date->currency }}</td>
+                <td>{{$date->single_price}} {{ $date->currency }}</td>
+                <td>
+                    <a name="" id="" class="btn btn-info" href="{{route('reservation.show', [$date->id])}}" role="button">Rezervasyon Yap</a>
+                </td>
+            </tr>
             @endforeach
         </table>
         @endif
