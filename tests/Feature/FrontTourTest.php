9544ae5a0ba352af5533d6fe8d8371a9ed366256
yalin <yalincobanoglu@yandex.com>
2018-04-24 17:24:33 +0300

    test

--- tests/Feature/FrontTourTest.php
+++ tests/Feature/FrontTourTest.php
@@ -5,18 +5,42 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Date;
 
 class FrontTourTest extends TestCase
 {
-    public function test_is_tour_detail_page_working()
+/*     public function test_is_tour_detail_page_working()
     {
-        $response = $this->get('/tour_detail/1');
+        $date = Date::first();
+        $response = $this->get('tour_detail/' . $date->id);
         $response->assertStatus(200);
-    }
+    } */
 
     public function test_is_tour_reservation_step2_is_working()
     {
         $response = $this->get('/reservation_step2/1/4');
         $response->assertStatus(200);
+    }
+
+    public function test_tour_reservation_information_validation()
+    {
+        $post_array = array(
+            "name" => "Yalın Çobanoğlu",
+            "email"  => "yalin@yalin.com",
+            "address"  => "Antalya TÜRKİYE",
+            "adult"  => 1,
+            "gender" => [
+                'mr'
+            ],
+            "participant" => [
+                "Yalın Cobanoğlu"
+            ]
+        );
+
+        $response = $this->json('POST', 'res_post', $post_array);
+       
+        $response->assertStatus(200);
+        $response->assertSessionHasErrors();
+        
     }
 }
