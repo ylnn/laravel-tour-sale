464d1cb4aaf27f92443cd74b51acddd1bb56d328
yalin <yalincobanoglu@yandex.com>
2018-04-25 14:56:49 +0300

    reservation table contact_id

--- database/migrations/2018_04_24_142306_create_reservations_table.php
+++ database/migrations/2018_04_24_142306_create_reservations_table.php
@@ -18,13 +18,14 @@ class CreateReservationsTable extends Migration
             $table->string('reservation_id', 70)->unique();
             $table->unsignedInteger('date_id');
             $table->unsignedInteger('tour_id');
-            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('contact_id');
             $table->unsignedInteger('pax');
             $table->unsignedInteger('price');
             $table->unsignedInteger('total_price');
             $table->string('currency');
             $table->integer('payment_status')->default(0);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
