cdbe45ec2ab49d1af8c361240978bd5efc0f7895
yalin <yalincobanoglu@yandex.com>
2018-05-01 20:24:49 +0300

    contacts table update

--- database/migrations/2018_04_25_115715_create_contacts_table.php
+++ database/migrations/2018_04_25_115715_create_contacts_table.php
@@ -15,6 +15,7 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('date_id');
             $table->string('name');
             $table->string('email');
             $table->text('address');
