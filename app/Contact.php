5de7ea9580718fb9cd02c3ba849ce58276d5e478
yalin <yalincobanoglu@yandex.com>
2018-05-01 20:24:16 +0300

    contact model

--- app/Contact.php
+++ app/Contact.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Date;
 
 class Contact extends Model
 {
-    //
+    public function date()
+    {
+        return $this->belongsTo(Date::class);   
+    }
 }
