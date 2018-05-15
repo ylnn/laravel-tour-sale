c754ee2a176f56e2f18b745ca6b9e1cb86d0c6fd
yalin <yalincobanoglu@yandex.com>
2018-05-15 21:37:36 +0300

    tour controller transaction

--- app/Http/Controllers/Front/TourController.php
+++ app/Http/Controllers/Front/TourController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Front;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 use App\Category;
 use App\Tour;
 use App\Date;
 use Validator;
 use App\Reservation;
 use App\User;
+use App\Pax;
 use App\Contact;
 
 class TourController extends Controller
@@ -57,9 +60,10 @@ class TourController extends Controller
         $validator = Validator::make($request->all(), [
             "name" => "string|required",
             "email" => "required|email",
+            "phone" => "string|required",
             "address" => "string|required",
             "pax_count" => "integer|required",
-            "pax.*.gender" => "string|required",
+            "pax.*.gender" => "string|required|in:mr,mrs,miss",
             "pax.*.name" => "string",
         ]);
 
@@ -72,22 +76,91 @@ class TourController extends Controller
         }
 
 
+        
         $date = $this->getDateWithId($request->date_id);
-
+        
+        // dd($date);
+        
         $available = $this->getDateAvailable($date);
-
-        if($available < $request->pax_count){
+ 
+        if($available < (int)$request->pax_count){
             return response()->json('not available', 400);
         } 
 
-       $pax = collect($request->pax); 
+        
+        
+        //TRANSACTION START
+        
+        DB::transaction(function () use ($date, $request) {
+            
+            $pax = collect($request->pax);
+       // Create Contact
+
+       $contact = new Contact();
+       $contact->date_id = $date->id;
+       $contact->name = $request->name;
+       $contact->email = $request->email;
+       $contact->address = $request->address;
+       $contact->phone = $request->phone;
+       $contact->country = 'default'; 
+
+       if($contact->save() == false){
+            return response()->json('contact save error', 400);
+       }
+
+       dump('contact saved');
+
+       $contact_id = $contact->id;
+
+    //    dd('contact saved');
+
+
+       // Create reservation
+       $reservation = new Reservation();
+       $reservation->date_id = $date->id;
+       $reservation->tour_id = $date->tour_id;
+       $reservation->contact_id = $contact_id;
+       $reservation->pax = (int)$request->pax_count;
+       $reservation->price = $date->price;
+
+       $total_price = (int)$request->pax_count * (int)$date->price;
+       
+       $reservation->total_price = $total_price;
+       $reservation->currency = $date->currency;
+       $reservation->payment_status = 0;
+
+
+       if($reservation->save() == false){
+            return response()->json('reservation save error', 400);
+       }
+
+       dump('res saved');
+       dump($reservation);
+
+       $reservation_id = $reservation->id;
+
+
+        $pax->each(function ($item, $key) use ($date, $reservation_id) {
+            print_r($item);
+            $newPax = new Pax();
+            $newPax->date_id = $date->id;
+            $newPax->reservation_id = $reservation_id;
+            $newPax->name = $item['name'];
+            $newPax->gender = $item['gender'];
+            $newPax->save();
+
+        });
+
+
+    }); // TRANSACTION END
+
+       dump('reservation saved.');
+
+       dd($reservation);
+
 
     //    dd($pax);
        
-       $pax->each(function($item, $key){
-           $contact = new Contact();
-           $contact->date_id = $date->id;
-        });
         
         dd('ok devam!');
         return $date;
