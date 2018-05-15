9cb5d8f91ddf53083e359080f727a516c24de954
yalin <yalincobanoglu@yandex.com>
2018-05-15 21:37:12 +0300

    reservation id to uuid

--- app/Reservation.php
+++ app/Reservation.php
@@ -4,9 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Reservation extends Model
 {
     use SoftDeletes;
-    //
+
+    public $incrementing = false;
+
+    /**
+     * Boot function from laravel.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = (string)Str::uuid();
+        });
+    }
 }
