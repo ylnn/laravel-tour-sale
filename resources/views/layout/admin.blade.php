d368f2cb4946e69c63f169a1f8856c00d647ebfd
yalin <yalincobanoglu@yandex.com>
2018-06-02 16:03:05 +0300

    payment link

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -69,7 +69,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">
+                            <a class="nav-link" href="{{ route('admin.payment.index') }}">
                                 <span data-feather="file-text"></span>
                                 Ödemeler
                             </a>
