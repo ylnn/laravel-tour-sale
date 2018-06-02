a5d26d2f225a0da07427860eaa6f212c591c2f0e
yalin <yalincobanoglu@yandex.com>
2018-06-02 12:48:17 +0300

    payments table

---
+++ database/migrations/2018_05_31_184127_create_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('res_id');
+            $table->integer('amount');
+            $table->string('currency');
+            $table->string('card_holder');
+            $table->string('card_no');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
