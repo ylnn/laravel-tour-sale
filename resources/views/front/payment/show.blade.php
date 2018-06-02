bb88ba5396bfd0cc237e7bb0b5eee9c46b16d689
yalin <yalincobanoglu@yandex.com>
2018-06-02 12:48:32 +0300

    payment show page alert added

--- resources/views/front/payment/show.blade.php
+++ resources/views/front/payment/show.blade.php
@@ -9,8 +9,17 @@
         <div>
             <h2>@lang('frontLang.reservation-saved-success')</h2>
         </div>
+
+        <div>
+            @if($errors->any())
+            <div class="alert alert-danger">
+                {{$errors->first()}}
+            </div>
+            @endif
+        </div>
         <br>
-        <form class="bg-light p-3" method="POST">
+        <form class="bg-light p-3" method="POST" action="{{route('payment.make', [$reservation->id])}}">
+            {{csrf_field()}}
             <div class="make-payment-title"> 
                 <h2>@lang('frontLang.make-payment')</h2>
             </div>
@@ -33,15 +42,15 @@
                         <div class="col-5">
                               <select class="form-control" name="month" id="month">
                                     <option value="0">@lang('frontLang.month')</option>
-                                    <option value="01">01</option>
-                                    <option value="02">02</option>
-                                    <option value="03">03</option>
-                                    <option value="04">04</option>
-                                    <option value="05">05</option>
-                                    <option value="06">06</option>
-                                    <option value="07">07</option>
-                                    <option value="08">08</option>
-                                    <option value="09">09</option>
+                                    <option value="1">01</option>
+                                    <option value="2">02</option>
+                                    <option value="3">03</option>
+                                    <option value="4">04</option>
+                                    <option value="5">05</option>
+                                    <option value="6">06</option>
+                                    <option value="7">07</option>
+                                    <option value="8">08</option>
+                                    <option value="9">09</option>
                                     <option value="10">10</option>
                                     <option value="11">11</option>
                                     <option value="12">12</option>
@@ -76,6 +85,7 @@
                     <button type="submit" class="btn btn-lg btn-success">Make payment</button>
                 </div>
             </div>
+
         </form>
 
     </div>
