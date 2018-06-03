4c774e1c34715986a98c3d463cc4445b61b70288
yalin <yalincobanoglu@yandex.com>
2018-06-03 08:54:49 +0300

    header links removed

--- resources/views/front/includes/header.blade.php
+++ resources/views/front/includes/header.blade.php
@@ -6,15 +6,12 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>TourSale Front</title>
-    <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}" >
     <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    <!-- Custom styles for this template -->
     <link href="{{asset('css/theme.css')}}" rel="stylesheet">
     <link href="{{asset('css/style.css')}}" rel="stylesheet">
     <link href="{{asset('css/lightslider.min.css')}}" rel="stylesheet">
     <link href="{{asset('css/summernote/summernote-bs4.css')}}" rel="stylesheet">
-    {{--  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">  --}}
     @stack('header_js')
 </head>
 <body>
@@ -23,33 +20,11 @@
         <header class="blog-header py-3">
             <div class="row flex-nowrap justify-content-between align-items-center">
                 <div class="col-4 pt-1">
-                    <a class="text-muted" href="#">Subscribe</a>
                 </div>
                 <div class="col-4 text-center">
-                    {{--  <a class="blog-header-logo text-dark" href="#">italya.org</a>  --}}
                     <a href="{{route('home')}}">
-                        <h1>TourSale</h1>
+                        <h1>TourSale With Laravel</h1>
                     </a>
-                </div>
-                <div class="col-4 d-flex justify-content-end align-items-center">
-                    
-                    <a class="text-muted" href="#">
-                        <i class="fa fa-user-circle-o" aria-hidden="true" style="padding-right:10px;"></i>
-                    </a>
-                    @auth
-
-                        <a href="{{ route('profile.show', ['id' => auth()->id()]) }}">
-                            {{ auth()->user()->username }}
-                        </a>
-                        <a name="" id="" class="btn btn-sm btn-outline-secondary" style="margin-left:10px;" href="{{route('auth.logout')}}" role="button">Çıkış</a>
-                    @endauth
-                
-                    @guest
-                    <a class="btn btn-sm btn-outline-secondary" href="{{ route('auth.loginform') }}">Giriş yap</a>
-                    &nbsp;
-                    <a class="btn btn-sm btn-outline-secondary" href="{{ route('auth.register') }}">Kayıt ol</a>
-                    @endguest
-
                 </div>
             </div>
         </header>
