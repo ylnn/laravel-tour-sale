182a16823c8b013c6fc01654e3e3054f546d54bc
yalin <yalincobanoglu@yandex.com>
2018-06-03 10:49:39 +0300

    lang vue update

--- resources/assets/js/components/PriceComponent.vue
+++ resources/assets/js/components/PriceComponent.vue
@@ -3,20 +3,20 @@
     <div>
 
                         <div class="form-group">
-                            <label for="adult">Kişi sayısı</label>
+                            <label for="adult">{{pax_count_string}}</label>
                             <select name="adult" id="adult" class="form-control" v-model.number="adult">
-                                <option value="1">1 Kişi</option>
-                                <option value="2">2 Kişi</option>
-                                <option value="3">3 Kişi</option>
-                                <option value="4">4 Kişi</option>
-                                <option value="5">5 Kişi</option>
+                                <option value="1">1 {{adult_string}}</option>
+                                <option value="2">2 {{adult_string}}</option>
+                                <option value="3">3 {{adult_string}}</option>
+                                <option value="4">4 {{adult_string}}</option>
+                                <option value="5">5 {{adult_string}}</option>
                             </select>
                         </div>
 
-                        <p>Total Price: {{currency}} <b>{{totalPrice}}</b></p>
+                        <p>{{total_price_string}}: {{currency}} <b>{{totalPrice}}</b></p>
 
                     <div style="padding-top:20px;">
-                        <button class="btn btn-success btn-lg" v-on:click="redirectToStep2">Devam Et</button>
+                        <button class="btn btn-success btn-lg" v-on:click="redirectToStep2">{{continue_string}}</button>
                     </div>
     </div>
 </template>
@@ -24,7 +24,16 @@
 <script>
     export default {
 
-        props : ['date_id', 'price', 'currency', 'step2_url'],
+        props : [
+            'date_id', 
+            'price', 
+            'currency', 
+            'step2_url',
+            'total_price_string',
+            'continue_string',
+            'pax_count_string',
+            'adult_string',
+            ],
 
         data() {
             return {
