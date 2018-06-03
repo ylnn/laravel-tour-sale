2423ee7c80eb6645271245d9f0a61cd064891123
yalin <yalincobanoglu@yandex.com>
2018-06-03 13:51:27 +0300

    users table clear

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->text('address');
             $table->rememberToken();
             $table->timestamps();
         });
