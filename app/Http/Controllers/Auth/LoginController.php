ba34dad83c33179ac25881d588235fd03925d47a
yalin <yalincobanoglu@yandex.com>
2018-06-03 13:58:45 +0300

    redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin/category/index';
 
     /**
      * Create a new controller instance.
