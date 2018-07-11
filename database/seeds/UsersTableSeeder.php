6cd42e47faf473d2cb239aa88bcda0fd5bd6bddd
yalin <yalincobanoglu@yandex.com>
2018-07-11 17:05:33 +0300

    users seeder update

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -7,13 +8,19 @@ class UsersTableSeeder extends Seeder
 {
     public function run()
     {
-        \DB::table('users')->insert([
-            'name' => 'Demo Admin',
-            'email' => 'admin@demo.com',
-            'password' => Hash::make('demo'),
-            'created_at' => \Carbon\Carbon::now(),
-            'updated_at' => \Carbon\Carbon::now(),
-        ]);
+        $users = User::all();
+
+        if($users->count() <> 1) {
+            \DB::table('users')->delete();
+            \DB::table('users')->insert([
+                'name' => 'Demo Admin',
+                'email' => 'admin@demo.com',
+                'password' => Hash::make('demo'),
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now(),
+            ]);
+        }
+
 
     }
 }
