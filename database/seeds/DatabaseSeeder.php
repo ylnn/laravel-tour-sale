4c39f83d91465963e38b4af6d3d18299894e3fd6
yalin <yalincobanoglu@yandex.com>
2018-07-11 17:05:06 +0300

    db seeder update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
-        // $this->call(ToursTableSeeder::class);
-        // $this->call(PhotoTourTableSeeder::class);
+        $this->call(ToursTableSeeder::class);
+        $this->call(PhotoTourTableSeeder::class);
         // $this->call(DatesTableSeeder::class);
         // $this->call(PhotosTableSeeder::class);
     }
